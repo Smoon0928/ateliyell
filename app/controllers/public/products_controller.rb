9556f6d541b2c9212fe8088a9a99df76b30ec3b1
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-03-06 09:30:16 +0000

    Add genre

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,19 +1,34 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products=Product.all
   end
 
   def new
+    @product=Product.new
   end
 
   def create
+    @product=Product.new(product_params)
+    if @product.save
+      redirect_to '/products'
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def show
+    @product=Product.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :introduction, :image)
+  end
+  
 end
