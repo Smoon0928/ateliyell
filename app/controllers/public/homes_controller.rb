8dfd03bb70075c636f631401bb9d4741f99d1dc3
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-03-19 16:18:23 +0000

    Fix layout top_page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @products = Product.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
   end
 
   def about
