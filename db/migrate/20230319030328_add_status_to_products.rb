51ee74c0e3219a7f131653548112449128c4a14f
EC2 Default User <ec2-user@ip-172-31-40-46.ap-northeast-1.compute.internal>
2023-03-26 07:31:42 +0000

    Fix migration file

--- db/migrate/20230319030328_add_status_to_products.rb
+++ db/migrate/20230319030328_add_status_to_products.rb
@@ -1,5 +1,5 @@
 class AddStatusToProducts < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :status, :integur,null: false,default: 0
+    add_column :products, :status, :integer,null: false,default: 0
   end
 end
